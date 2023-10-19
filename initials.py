a29cb5c736d6de25516ea46032272fde73a06456
Muhamad Muhsin <83757177+mohamuhsin@users.noreply.github.com>
2023-10-19 15:54:40 +0300

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,1 +1,17 @@
+# Your first and last name.
+# A fun fact about yourself.
+print("  SSS  ")
+print(" S   S ")
+print(" S     ")
+print("  SSS  ")
+print("     S ")
+print(" S   S ")
+print("  SSS  ")
 
+print("  SSS   L     ")
+print(" S   S  L     ")
+print(" S      L     ")
+print("  SSS   L     ")
+print("     S  L     ")
+print(" S   S  L     ")
+print("  SSS   LLLLL ")
