b8beca479d027f6f60ba1812b4fb13333744a8dd
Muhamad Muhsin <83757177+mohamuhsin@users.noreply.github.com>
2023-10-19 16:27:23 +0300

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,17 +1,41 @@
-# Your first and last name.
-# A fun fact about yourself.
-print("  SSS  ")
-print(" S   S ")
-print(" S     ")
-print("  SSS  ")
-print("     S ")
-print(" S   S ")
-print("  SSS  ")
+lovely_loveseat_description = '''
+Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white.
+'''
 
-print("  SSS   L     ")
-print(" S   S  L     ")
-print(" S      L     ")
-print("  SSS   L     ")
-print("     S  L     ")
-print(" S   S  L     ")
-print("  SSS   LLLLL ")
+lovely_loveseat_price = 254.00;
+
+stylish_settee_description = '''
+Stylish Settee. Faux leather on birch. 29.50 inches high x 54.75 inches wide x 28 inches deep. Black.
+'''
+
+stylish_settee_price = 180.50; 
+
+luxurious_lamp_description = '''
+Luxurious Lamp. Glass and iron. 36 inches tall. Brown with cream shade.
+'''
+
+luxurious_lamp_price = 52.15;
+
+sales_tax = .088;
+
+customer_one_total = 0;
+
+customer_one_itemization = '';
+
+customer_one_total += lovely_loveseat_price;
+
+customer_one_itemization += lovely_loveseat_description;
+
+customer_one_total += luxurious_lamp_price;
+
+customer_one_itemization += luxurious_lamp_description;
+
+customer_one_tax = customer_one_total * sales_tax;
+
+customer_one_total += customer_one_tax;
+
+#cusromer items
+print(customer_one_itemization);
+
+#cusromer total
+print(customer_one_total);
